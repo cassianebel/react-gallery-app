8f18ecb7a80374fea128edcb10ab06da1c584690
Cassia Nebel <162936759+cassianebel@users.noreply.github.com>
2024-05-24 09:00:59 -0500

    initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react'
+import { Route, Routes, Navigate } from "react-router-dom";
+import apiKey from './config.js'
+import Nav from './components/Nav'
+import Search from './components/Search'
+import PhotoList from './components/PhotoList'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="container">
+      <Search />
+      <Nav />
+      <Routes>
+        <Route path="/" element={<PhotoList />}>
+          <Route index element={<Navigate replace to="cats" />} />
+        </Route>
+        <Route path="cats" element={<PhotoList />} />
+        <Route path="dogs" element={<PhotoList />} />
+        <Route path="computers" element={<PhotoList />} />
+        <Route path="search/:query" element={<PhotoList />} />
+      </Routes>
+      
+    </div>
   )
 }
 
---
+++ src/components/PhotoList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Photo from './Photo';
+
+const PhotoList = () => {
+
+  return (
+    <div className="photo-container">
+      <h2>Results</h2>
+      <ul>
+      </ul>
+    </div>
+  )
+}
+
+export default PhotoList
