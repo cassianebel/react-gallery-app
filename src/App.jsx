6512fdbfad0a29ccd1040dccb36ba21444aa7cd7
Cassia Nebel <162936759+cassianebel@users.noreply.github.com>
2024-05-24 14:49:33 -0500

    fetching the data

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,35 @@
 import React, { useState, useEffect } from 'react'
 import { Route, Routes, Navigate } from "react-router-dom";
+import axios from 'axios';
 import apiKey from './config.js'
 import Nav from './components/Nav'
 import Search from './components/Search'
 import PhotoList from './components/PhotoList'
 
 function App() {
-
+  const [photos, setPhotos] = useState([]);
+ 
+  const fetchData = async (query) => {
+    try {
+      const response = await axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`);
+      setPhotos(response.data.photos.photo);
+    } catch (error) {
+      console.log('Error fetching and parsing data', error);
+    }
+  }
   
   return (
     <div className="container">
       <Search />
       <Nav />
       <Routes>
-        <Route path="/" element={<PhotoList />}>
+        <Route path="/" element={<PhotoList photos={photos} fetchData={fetchData} />}>
           <Route index element={<Navigate replace to="cats" />} />
         </Route>
-        <Route path="cats" element={<PhotoList />} />
-        <Route path="dogs" element={<PhotoList />} />
-        <Route path="computers" element={<PhotoList />} />
-        <Route path="search/:query" element={<PhotoList />} />
+        <Route path="cats" element={<PhotoList photos={photos} query="cats" fetchData={fetchData} />} />
+        <Route path="dogs" element={<PhotoList photos={photos} query="dogs" fetchData={fetchData} />} />
+        <Route path="computers" element={<PhotoList photos={photos} query="computers" fetchData={fetchData} />} />
+        <Route path="search/:query" element={<PhotoList photos={photos} fetchData={fetchData} />} />
       </Routes>
       
     </div>
--- src/components/PhotoList.jsx
+++ src/components/PhotoList.jsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import PropTypes from 'prop-types';
 import Photo from './Photo';
 
-const PhotoList = () => {
+const PhotoList = ({ photos, query, fetchData }) => {
+  useEffect(() => {
+    fetchData(query);
+  }, [query, fetchData]);
 
   return (
     <div className="photo-container">
       <h2>Results</h2>
       <ul>
+        {photos.map(photo => (
+          <Photo
+            key={photo.id}
+            url={`https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`}
+          />
+        ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default PhotoList
+PhotoList.propTypes = {
+  photos: PropTypes.array.isRequired,
+  query: PropTypes.string.isRequired,
+  fetchData: PropTypes.func.isRequired
+};
+
+export default PhotoList;
